15931e1621f94b0fe78512dcd2a3b86ae8f02b72
Drew Lofland <alofland25@gmail.com>
2022-12-30 12:18:55 -0500

    finished selection statements exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favNumber = 7;
+            
+            Console.WriteLine("Try to guess the number.Please enter a number between 1 and 10");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("That number is too low.");
+                
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("That number is too high");
+            }
+            else 
+            {
+                Console.WriteLine("Congrats! You guessed it");
+            }
+            {
+
+            }
         }
     }
 }
